1dcc7c6ae171a9f9f8a28c6fd67e30b24120cc62
Kyle Chapman <splash004@gmail.com>
2018-09-18 16:10:45 -0700

    server started

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 
+const express = require('express');
 
-console.log('hhhi');
+const app = express();
+const router = express.Router();
+app.use('/a', router);
+
+
+router.get('/b', (req, res) =>{
+
+  console.log('haaai')
+})
+
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
